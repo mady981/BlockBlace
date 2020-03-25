4a864e6ff8644c374cf1949c3efa921b0dca7fcb
mady981 <moritz-eibl@web.de>
2020-03-25 12:03:23 +0100

    drawrectdim with vector and fixed drawrectdim;

--- Engine/Graphics.h
+++ Engine/Graphics.h
@@ -24,6 +24,7 @@
 #include <wrl.h>
 #include "ChiliException.h"
 #include "Colors.h"
+#include "Vector.h"
 
 class Graphics
 {
@@ -58,9 +59,13 @@ public:
 	}
 	void PutPixel( int x,int y,Color c );
 	void DrawRect( int x0 , int y0 , int x1 , int y1 , Color c );
-	void DrawRectDim( int x0 , int y0 , int height , int width , Color c )
+	void DrawRectDim( int x0 , int y0 , int width , int height , Color c )
 	{
-		DrawRect( x0,y0,x0 + height,y0 + width, c );
+		DrawRect( x0,y0,x0 + width,y0 + height, c );
+	}
+	void DrawRectDim( const Vector& topleft,int width,int height,Color c )
+	{
+		DrawRectDim( topleft.x,topleft.y,width,height,c );
 	}
 	~Graphics();
 private:
